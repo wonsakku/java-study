24b4909283ee7a1464549dc0fdba8083e2fa3042
wonsakku <kws1044@gmail.com>
2023-04-20 22:32:35 +0900

    5장. 메서드 추출

---
+++ java_refactoring_for_beginner/code/src/main/java/chap05/_5_2_exam_program/_1_before_refactoring/Banner.java
@@ -0,0 +1,31 @@
+package chap05._5_2_exam_program._1_before_refactoring;
+
+public class Banner {
+
+    private final String _content;
+
+    public Banner(String _content) {
+        this._content = _content;
+    }
+
+    public void print(int times){
+        // 테두리 출력
+        System.out.print("+");
+        for(int i = 0 ; i < _content.length() ; i++){
+            System.out.print("-");
+        }
+        System.out.println("+");
+
+        // 내용 출력
+        for(int i = 0 ; i < times ; i++){
+            System.out.println("|" + _content + "|");
+        }
+
+        // 테두리 출력
+        System.out.print("+");
+        for(int i = 0 ; i < _content.length() ; i++){
+            System.out.print("-");
+        }
+        System.out.println("+");
+    }
+}
